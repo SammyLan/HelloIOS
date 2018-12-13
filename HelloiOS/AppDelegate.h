050b9ee21c891dcf48a5efb96addd335fbc576b8
SammyLan <sammylan234@gmail.com>
2018-12-13 16:57:55 +0800

    base version

---
+++ HelloiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloiOS
+//
+//  Created by 蓝秀文 on 2018/12/13.
+//  Copyright © 2018 蓝秀文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloiOS
+//
+//  Created by 蓝秀文 on 2018/12/13.
+//  Copyright © 2018 蓝秀文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
