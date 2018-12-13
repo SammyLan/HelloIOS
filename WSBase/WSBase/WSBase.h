5522e1243ebfed633596214aac9b080d0d2fc1b5
SammyLan <sammylan234@gmail.com>
2018-12-13 19:33:38 +0800

    Base and Login Framework

---
+++ WSBase/WSBase/WSBase.h
@@ -0,0 +1,19 @@
+//
+//  WSBase.h
+//  WSBase
+//
+//  Created by 蓝秀文 on 2018/12/13.
+//  Copyright © 2018 蓝秀文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WSBase.
+FOUNDATION_EXPORT double WSBaseVersionNumber;
+
+//! Project version string for WSBase.
+FOUNDATION_EXPORT const unsigned char WSBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WSBase/PublicHeader.h>
+
+
